e1e29e42c880961c1de551a4d0524a51aa9d8b7e
John <souzajohnlee@gmail.com>
2021-09-27 13:06:18 -0700

    show skateboard details

---
+++ SkateboardShowPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function SkateboardShowPage() {
+    return (
+        <div>
+            <h1>Skateboard 1</h1>
+            <p>This skateboard rocks!!!</p>
+        </div>
+    )
+}
